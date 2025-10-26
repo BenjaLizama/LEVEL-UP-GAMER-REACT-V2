f45164198eb2d79908aabb7e2275d2f253f13f38
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-10-26 13:55:17 -0300

    style: Actualizacion de estilos globales

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import "@/App.css";
+import "@/styles/global.css";
 import Home from "@/components/pages/Home";
 import Marketplace from "@/components/pages/Marketplace";
 import Cart from "@/components/pages/Cart";
 import Profile from "@/components/pages/Profile";
 import { Routes, Route } from "react-router-dom";
 import Test from "./components/pages/Test";
+import { useEffect, useState } from "react";
+import Button from "./components/atoms/Button/Button";
 
 export default function App() {
+  const [modoClaro, setModoClaro] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("modoClaro", modoClaro);
+  }, [modoClaro]);
+
   return (
     <div>
+      <Button onClick={() => setModoClaro(!modoClaro)}>
+        Cambiar a {modoClaro ? "modo oscuro" : "modo claro"}
+      </Button>
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/marketplace" Component={Marketplace} />
