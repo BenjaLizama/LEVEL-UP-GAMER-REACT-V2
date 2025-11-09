c2401b3ed89dc9ba4a6a7af172ce0826c5b8817b
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-09 17:20:06 -0300

    refacto: Refactorizacion dentro de pagina Tienda

--- src/components/pages/Marketplace.tsx
+++ src/components/pages/Marketplace.tsx
@@ -1,3 +1,52 @@
+import { useEffect, useState } from "react";
+import { Producto } from "@/models/Producto";
+import { productoService } from "@/services/ProductoService";
+import ProductGrid from "../organisms/ProductGrid/ProductGrid";
+
 export default function Marketplace() {
-  return <div>Marketplace</div>;
+  const [productos, setProductos] = useState<Producto[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetchProductos();
+  }, []);
+
+  const fetchProductos = async () => {
+    try {
+      setIsLoading(true);
+      const data = await productoService.getAllProducts();
+      setProductos(data);
+      setError(null);
+    } catch {
+      setError(
+        "No se pudieron cargar los productos. Intenta nuevamente mas tarde"
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        Cargando catálogo...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div style={{ color: "red", textAlign: "center", marginTop: "50px" }}>
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      <h1>Nuestra Tienda</h1>
+      <ProductGrid productos={productos} />
+    </main>
+  );
 }
