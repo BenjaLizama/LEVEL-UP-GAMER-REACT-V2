85534b09597e5c160c730434d9df3ae17caaa497
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-14 16:08:25 -0300

    refactor: Refactoriza pagina Store

---
+++ src/components/pages/Store/Store.tsx
@@ -0,0 +1,125 @@
+import { useEffect, useMemo, useState } from "react";
+import styles from "./Store.module.css";
+import { Producto } from "@/models/Producto";
+import { productoService } from "@/services/ProductoService";
+import ProductGrid from "../../organisms/ProductGrid/ProductGrid";
+import CategoryFilter from "../../molecules/CategoryFilter/CategoryFilter";
+import { CATEGORIAS_POR_DEFECTO } from "@/models/Categoria";
+import SimpleInput from "@/components/atoms/SimpleInput/SimpleInput";
+import { InputType } from "@/components/atoms/SimpleInput/SimpleInput.types";
+
+export default function Store() {
+  const [productos, setProductos] = useState<Producto[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const [categoriaSeleccionada, setCategoriaSeleccionada] =
+    useState<string>("");
+
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  useEffect(() => {
+    const fetchProductosActualizados = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+        let data: Producto[];
+
+        if (categoriaSeleccionada === "") {
+          data = await productoService.getAllProducts();
+        } else {
+          data = await productoService.filtrarProductosPorCategoria(
+            categoriaSeleccionada
+          );
+        }
+
+        setProductos(data);
+      } catch {
+        setError(
+          "No se pudieron cargar los productos. Intenta nuevamente mas tarde."
+        );
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProductosActualizados();
+  }, [categoriaSeleccionada]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const productosFiltrados = useMemo(() => {
+    if (!searchTerm) {
+      return productos;
+    }
+
+    return productos.filter((producto) =>
+      producto.nombreProducto.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [productos, searchTerm]);
+
+  if (isLoading) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <div className={styles.searchContainer}>
+          <SimpleInput
+            type={InputType.Search}
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Buscar por nombre..."
+            disabled
+          />
+          <CategoryFilter
+            categorias={CATEGORIAS_POR_DEFECTO}
+            categoriaSeleccionada={categoriaSeleccionada}
+            onCategoriaChange={setCategoriaSeleccionada}
+          />
+        </div>
+        Cargando catálogo...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div style={{ color: "red", textAlign: "center", marginTop: "50px" }}>
+        <div className={styles.searchContainer}>
+          <SimpleInput
+            type={InputType.Search}
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Buscar por nombre..."
+          />
+          <CategoryFilter
+            categorias={CATEGORIAS_POR_DEFECTO}
+            categoriaSeleccionada={categoriaSeleccionada}
+            onCategoriaChange={setCategoriaSeleccionada}
+          />
+        </div>
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      <div className={styles.searchContainer}>
+        <SimpleInput
+          className={styles.simpleInput}
+          type={InputType.Search}
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Buscar por nombre..."
+        />
+        <CategoryFilter
+          categorias={CATEGORIAS_POR_DEFECTO}
+          categoriaSeleccionada={categoriaSeleccionada}
+          onCategoriaChange={setCategoriaSeleccionada}
+        />
+      </div>
+      <ProductGrid productos={productosFiltrados} />
+    </main>
+  );
+}
