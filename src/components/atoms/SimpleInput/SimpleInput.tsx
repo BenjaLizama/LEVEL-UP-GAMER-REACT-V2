034997b13ed359581be0bbe4cb9ec587d53e1254
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-14 16:05:42 -0300

    style: Modifica el estilo de SimpleInput

--- src/components/atoms/SimpleInput/SimpleInput.tsx
+++ src/components/atoms/SimpleInput/SimpleInput.tsx
@@ -6,6 +6,7 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   value: string;
   type: InputType;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  className: string;
 }
 
 export default function SimpleInput({ className, ...rest }: Props) {
