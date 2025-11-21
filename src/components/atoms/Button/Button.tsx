35c8c0b7a92a69c8932c39f99c42d9c123e4dc14
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-21 14:55:47 -0300

    refactor: Refactoriza componente Button

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -4,13 +4,19 @@ interface Props {
   children: React.ReactNode;
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 }
 
 export default function Button(props: Props) {
-  const { children, onClick, type = "button" } = props;
+  const { children, onClick, type = "button", disabled = false } = props;
 
   return (
-    <button className={styles.button} onClick={onClick} type={type}>
+    <button
+      className={styles.button}
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
