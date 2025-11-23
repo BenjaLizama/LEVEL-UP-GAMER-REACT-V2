15cea5c8d53ede2cd5015b2268d93978dde61945
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-22 23:32:39 -0300

    feat: Implementa nuevos iconos

--- src/utils/Icons.ts
+++ src/utils/Icons.ts
@@ -1,12 +1,13 @@
 const HOME = "ls:home";
 const STORE = "streamline-plump:store-2-solid";
-const LINK_EXTERNAL = "rivet-icons:link-external";
+const LINK_EXTERNAL = "iconamoon:link-external-fill";
 const ADD_CART = "solar:cart-plus-bold";
 const CART_ARROW_DOWN = "ps:shopping-cart";
 const X = "tabler:x";
 const EMAIL = "dashicons:email";
 const KEY = "mdi:key";
 const USER = "gridicons:user";
+const ARROW = "material-symbols:arrow-drop-down-rounded";
 
 export {
   HOME,
@@ -18,4 +19,5 @@ export {
   EMAIL,
   KEY,
   USER,
+  ARROW,
 };
