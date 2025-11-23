878870dd0d4a05bfa2b5317e9afb5bed24382fdf
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-23 01:59:23 -0300

    style: Implementa estilos para pagina Profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,12 +1,34 @@
+import React from "react";
+import styles from "./Profile.module.css";
 import LoginProfileInfo from "@/components/molecules/LoginProfileInfo/LoginProfileInfo";
+import OptionSelector from "@/components/molecules/OptionSelector/OptionSelector";
+import OptionSelectorContainer from "@/components/organisms/OptionSelectorContainer/OptionSelectorContainer";
+import { ConfiguracionGeneral } from "@/constants/configOptions";
 
 export default function Profile() {
   const idUsuario = localStorage.getItem("idUsuario");
 
   return (
-    <div>
+    <div className={styles.main}>
       {!idUsuario && <LoginProfileInfo />}
-      <hr />
+      {idUsuario && (
+        <div className={styles.container}>
+          {ConfiguracionGeneral.map((organismo) => (
+            <React.Fragment key={organismo.organismoId}>
+              <OptionSelectorContainer descripcion={organismo.titulo}>
+                {organismo.opciones.map((opcion) => (
+                  <OptionSelector
+                    key={opcion.id}
+                    icono={opcion.icono}
+                    descripcion={opcion.descripcion}
+                    onClick={opcion.onClick}
+                  />
+                ))}
+              </OptionSelectorContainer>
+            </React.Fragment>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
