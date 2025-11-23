137a5fa2686e6659a082b087ca593c1d995afeb2
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-23 00:19:05 -0300

    refactor: Refactoriza componente OptionSelector

--- src/components/molecules/OptionSelector/OptionSelector.spec.tsx
+++ src/components/molecules/OptionSelector/OptionSelector.spec.tsx
@@ -1,14 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import OptionSelector from "./OptionSelector";
 import { LINK_EXTERNAL } from "@/utils/Icons";
 
-interface SimpleIconProps {
+interface MockSimpleIconProps {
   icon: string;
   fontSize: number;
 }
 
 jest.mock("@/components/atoms/SimpleIcon/SimpleIcon", () => {
-  return function MockSimpleIcon({ icon, fontSize }: SimpleIconProps) {
+  return function MockSimpleIcon({ icon, fontSize }: MockSimpleIconProps) {
     return (
       <div
         data-testid="mock-simple-icon"
@@ -25,26 +26,38 @@ jest.mock("@/components/atoms/SimpleIcon/SimpleIcon", () => {
 const mockProps = {
   icono: "TEST_ICONO_DINAMICO",
   descripcion: "Seleccionar idioma o región",
+  ruta: "/configuracion",
+  onClick: jest.fn(),
 };
 
 const setup = (props = mockProps) => {
-  return render(<OptionSelector {...props} />);
+  return render(
+    <BrowserRouter>
+      <OptionSelector {...props} />
+    </BrowserRouter>
+  );
 };
 
 describe("OptionSelector", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("Renderiza correctamente la descripción pasada por props", () => {
     setup();
-
     expect(screen.getByText(mockProps.descripcion)).toBeInTheDocument();
   });
 
   test("Renderiza el SimpleIcon dinámico con el icono y tamaño correctos", () => {
     setup();
 
-    const dynamicIcon = screen.getByText(`Mock Icono ${mockProps.icono}`);
+    const dynamicIconInstances = screen.getAllByTestId("mock-simple-icon");
+
+    const dynamicIcon = dynamicIconInstances.find(
+      (element) => element.getAttribute("data-icon") === mockProps.icono
+    );
 
     expect(dynamicIcon).toBeInTheDocument();
-
     expect(dynamicIcon).toHaveAttribute("data-icon", mockProps.icono);
     expect(dynamicIcon).toHaveAttribute("data-fontsize", "20");
   });
@@ -52,10 +65,13 @@ describe("OptionSelector", () => {
   test("Renderiza el SimpleIcon estático (flecha) con el icono y tamaño correctos", () => {
     setup();
 
-    const staticIcon = screen.getByText(`Mock Icono ${LINK_EXTERNAL}`);
+    const staticIconInstances = screen.getAllByTestId("mock-simple-icon");
+
+    const staticIcon = staticIconInstances.find(
+      (element) => element.getAttribute("data-icon") === LINK_EXTERNAL
+    );
 
     expect(staticIcon).toBeInTheDocument();
-
     expect(staticIcon).toHaveAttribute("data-icon", LINK_EXTERNAL);
     expect(staticIcon).toHaveAttribute("data-fontsize", "25");
   });
@@ -65,5 +81,13 @@ describe("OptionSelector", () => {
 
     const iconInstances = screen.getAllByTestId("mock-simple-icon");
     expect(iconInstances).toHaveLength(2);
+  });
+
+  test("El componente Link apunta a la ruta correcta", () => {
+    setup();
+    const linkElement = screen.getByRole("link");
+
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute("href", mockProps.ruta);
   });
 });
--- src/components/molecules/OptionSelector/OptionSelector.tsx
+++ src/components/molecules/OptionSelector/OptionSelector.tsx
@@ -2,18 +2,27 @@ import React from "react";
 import styles from "./OptionSelector.module.css";
 import SimpleIcon from "@/components/atoms/SimpleIcon/SimpleIcon";
 import { LINK_EXTERNAL } from "@/utils/Icons";
+import { Link } from "react-router-dom";
 
 interface OptionSelectorProps {
   icono: string;
   descripcion: string;
+  ruta?: string;
+  onClick?: () => void;
 }
 
 export default function OptionSelector({
   icono,
   descripcion,
+  ruta,
+  onClick,
 }: OptionSelectorProps) {
   return (
-    <div className={styles.optionSelectorContainer}>
+    <Link
+      className={styles.optionSelectorContainer}
+      to={ruta ? ruta : ""}
+      onClick={onClick}
+    >
       <div className={styles.optionIcon}>
         <SimpleIcon icon={icono} fontSize={20} />
       </div>
@@ -21,6 +30,6 @@ export default function OptionSelector({
       <div className={styles.optionArrow}>
         <SimpleIcon icon={LINK_EXTERNAL} fontSize={25} />
       </div>
-    </div>
+    </Link>
   );
 }
