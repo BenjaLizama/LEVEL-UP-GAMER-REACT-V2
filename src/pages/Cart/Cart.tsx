5ef09fdaa3031b0e350acb2865218e1d817314a4
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-11-28 14:10:19 -0300

    refactor: Refactoriza pagina Cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,3 +1,34 @@
+import React, { useEffect, useState, useMemo } from "react";
+import styles from "./Cart.module.css";
+import { Producto } from "@/models/Producto";
+import { productoService } from "@/services/ProductoService";
+import { enriqucerCarrito } from "@/utils/CarritoHelper";
+import { useCartData } from "@/utils/useCartData";
+import CartTemplate from "@/components/templates/CartTemplate/CartTemplate";
+
 export default function Cart() {
-  return <div>Cart</div>;
+  const { itemCart, total } = useCartData();
+  const [productos, setProductos] = useState<Producto[]>([]);
+
+  useEffect(() => {
+    const fetchProductos = async () => {
+      try {
+        const listaProductos = await productoService.getAllProducts();
+        setProductos(listaProductos);
+      } catch (error) {
+        console.error("Error cargando productos:", error);
+      }
+    };
+    fetchProductos();
+  }, []);
+
+  const itemsParaMostrar = useMemo(() => {
+    return enriqucerCarrito(itemCart, productos);
+  }, [itemCart, productos]);
+
+  return (
+    <div className={styles.container}>
+      <CartTemplate itemlist={itemsParaMostrar} total={total}></CartTemplate>
+    </div>
+  );
 }
