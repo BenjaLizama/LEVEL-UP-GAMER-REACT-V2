b02173fc4b3b2ec5a044f210ac5cdd7aa77810dc
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-12-04 01:57:59 -0300

    refactor: Refactoriza CartTemplate

--- src/components/templates/CartTemplate/CartTemplate.tsx
+++ src/components/templates/CartTemplate/CartTemplate.tsx
@@ -3,7 +3,10 @@ import CartItem from "@/components/molecules/CartItem/CartItem";
 import OrderSummary from "@/components/molecules/OrderSummary/OrderSummary";
 import styles from "./CartTemplate.module.css";
 import { formatearACLP } from "@/utils/Funciones";
+import CartControlButtons from "@/components/molecules/CartControlButtons/CartControlButtons";
+
 interface plantillaCartProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   itemlist: Array<any>;
   total: number;
 }
@@ -16,12 +19,17 @@ export default function CartTemplate({ itemlist, total }: plantillaCartProps) {
           <CartItem
             key={prod.codigoProducto}
             nombre={prod.nombre}
-            precio={prod.precioUnitario}
+            precio={formatearACLP(prod.precioUnitario)}
             cantidad={prod.cantidad}
             idItem={prod.codigoProducto}
             imagen={prod.img}
+            categoria={prod.categoria}
           >
-            <button>hola soy un boton</button>
+            <CartControlButtons
+              cantidad={prod.cantidad}
+              onAddClick={() => {}}
+              onRemoveClick={() => {}}
+            />
           </CartItem>
         ))}
       </section>
