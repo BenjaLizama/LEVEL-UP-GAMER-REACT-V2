2a788194a7968f8fd524bc7d4ef70de71420af6c
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-12-04 22:57:15 -0300

    refactor: Refactoriza componente ProductGrid

--- src/components/organisms/ProductGrid/ProductGrid.tsx
+++ src/components/organisms/ProductGrid/ProductGrid.tsx
@@ -5,6 +5,8 @@ import ProductCard from "@/components/molecules/ProductCard/ProductCard";
 import { carritoService } from "@/services/CarritoService";
 import Empty from "@/components/atoms/Empty/Empty";
 import { NO_PRODUCTS } from "@/utils/Icons";
+import { TemporaryAlert } from "@/components/molecules/AlertBox/AlertBox";
+import { useAlertQueue } from "@/hooks/useAlertQueue";
 
 interface ProductoGridProps {
   productos: Producto[];
@@ -12,6 +14,8 @@ interface ProductoGridProps {
 }
 
 export default function ProductGrid({ productos }: ProductoGridProps) {
+  const { currentAlert, handleShowAlert, handleCloseAlert } = useAlertQueue();
+
   if (!productos || productos.length === 0) {
     return (
       <div className={styles.noProductContainer}>
@@ -21,16 +25,38 @@ export default function ProductGrid({ productos }: ProductoGridProps) {
   }
 
   return (
-    <div className={styles.gridContainer}>
-      {productos.map((producto) => (
-        <ProductCard
-          key={producto.codigoProducto}
-          {...producto}
-          onAddCart={() =>
-            carritoService.addItemToCart(producto.codigoProducto, 1)
-          }
+    <>
+      <div className={styles.gridContainer}>
+        {productos.map((producto) => (
+          <ProductCard
+            key={producto.codigoProducto}
+            {...producto}
+            onAddCart={() => {
+              if (localStorage.getItem("idUsuario") != null) {
+                carritoService.addItemToCart(producto.codigoProducto, 1);
+                handleShowAlert(
+                  `Se agregó ${producto.nombreProducto} al carrito`,
+                  "success"
+                );
+              } else {
+                handleShowAlert(
+                  "Debes iniciar sesión para realizar esta acción",
+                  "error"
+                );
+              }
+            }}
+          />
+        ))}
+      </div>
+      {currentAlert && (
+        <TemporaryAlert
+          key={currentAlert.id}
+          message={currentAlert.message}
+          type={currentAlert.type as "success" | "error" | "warning"}
+          durationMs={1000}
+          onClose={handleCloseAlert}
         />
-      ))}
-    </div>
+      )}
+    </>
   );
 }
