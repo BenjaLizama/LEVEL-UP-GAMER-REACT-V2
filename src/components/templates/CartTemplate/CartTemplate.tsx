265d12e65d50e2efb8cd2eb2f5d79f2e5d5cbe1f
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-12-04 22:47:31 -0300

    reafctor: Refactoriza template CartTemplate

--- src/components/templates/CartTemplate/CartTemplate.tsx
+++ src/components/templates/CartTemplate/CartTemplate.tsx
@@ -4,6 +4,8 @@ import OrderSummary from "@/components/molecules/OrderSummary/OrderSummary";
 import styles from "./CartTemplate.module.css";
 import { formatearACLP } from "@/utils/Funciones";
 import CartControlButtons from "@/components/molecules/CartControlButtons/CartControlButtons";
+import { carritoService } from "@/services/CarritoService";
+import { usuarioService } from "@/services/UsuarioService";
 
 interface plantillaCartProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,6 +14,35 @@ interface plantillaCartProps {
 }
 
 export default function CartTemplate({ itemlist, total }: plantillaCartProps) {
+  const handleAddClick = (codigoProducto: string) => {
+    carritoService.addItemToCart(codigoProducto, 1);
+  };
+
+  const handleRemoveClick = (codigoProducto: string) => {
+    carritoService.removeItemCart(codigoProducto);
+  };
+
+  const handleDeleteClick = (codigoProducto: string) => {
+    carritoService.deleteItemCart(codigoProducto);
+  };
+
+  const handleOnPagarClick = async () => {
+    try {
+      const respuestaPago = await usuarioService.pagar();
+
+      const pagoUrl = respuestaPago.url;
+
+      if (pagoUrl) {
+        window.location.href = pagoUrl;
+      } else {
+        alert("Error: No se pudo obtener la URL de pago");
+      }
+    } catch (error) {
+      console.error("Error al iniciar el pago:", error);
+      alert(error);
+    }
+  };
+
   return (
     <>
       <section className={styles.seccionItems}>
@@ -27,15 +58,19 @@ export default function CartTemplate({ itemlist, total }: plantillaCartProps) {
           >
             <CartControlButtons
               cantidad={prod.cantidad}
-              onAddClick={() => {}}
-              onRemoveClick={() => {}}
+              onAddClick={() => handleAddClick(prod.codigoProducto)}
+              onRemoveClick={() => handleRemoveClick(prod.codigoProducto)}
+              onDeleteClick={() => handleDeleteClick(prod.codigoProducto)}
             />
           </CartItem>
         ))}
       </section>
 
       <div className={styles.orderSummary}>
-        <OrderSummary total={formatearACLP(total)}></OrderSummary>
+        <OrderSummary
+          total={formatearACLP(total)}
+          onPagar={() => handleOnPagarClick()}
+        ></OrderSummary>
       </div>
     </>
   );
